b004082be1aaf85c11e1a5482765dee50bff9098
Dane Falberg <70166906+pricelowtohigh@users.noreply.github.com>
2024-05-30 17:34:58 -0400

    React PDF Demo Invoice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import {PDFViewer} from '@react-pdf/renderer'
+import Invoice from './components/Invoice'
+import invoice from './data/invoice'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import logo from './logo.svg';
+import './App.css';
+class App extends Component {
+  
+  render() {
+    return (
+        <Fragment>
+            <PDFViewer width="1000" height="600" className="app" >
+                <Invoice invoice={invoice}/>
+            </PDFViewer>
+        </Fragment>
+    );
+  }
 }
 
 export default App;
